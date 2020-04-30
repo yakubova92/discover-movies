90abb9bfc13a49259a7d9297f000e0c331aa036c
Alla Yakubova <yakubova92@gmail.com>
2020-04-29 21:10:07 -0400

    api connected to movieDB

--- server/api/routes/movies.js
+++ server/api/routes/movies.js
@@ -1,24 +1,28 @@
 const express = require('express')
+const fetch = require('node-fetch')
 const router = express.Router()
+
+const { API_URL } = require('../../constants')
+const { api_key } = require('../../secrets')
 
 router.get('/', (req, res, next) => {
   const title = req.query.title
-  res.status(200).json({
-    message: 'get search results'
-  })
+  return fetch(`${API_URL}/search/movie?api_key=${api_key}&language=en-US&query=${title}&page=1&include_adult=false`)
+    .then(res => res.json())
+    .then(json => res.send(json));
 })
 
 router.get('/popular', (req, res, next) => {
-  res.status(200).json({
-    message: 'get popular movies'
-  })
+  return fetch(`${API_URL}/movie/popular?api_key=${api_key}&language=en-US&page=1`)
+    .then(res => res.json())
+    .then(json => res.send(json));
 })
 
 router.get('/:movieId', (req, res, next) => {
-  const { movieId } = req.params.movieId
-  res.status(200).json({
-    message: 'get movie details'
-  })
+  const { movieId } = req.params
+  return fetch(`${API_URL}/movie/${movieId}?api_key=${api_key}`)
+    .then(res => res.json())
+    .then(json => res.send(json));
 })
 
 module.exports = router
---
+++ server/constants.js
@@ -0,0 +1,5 @@
+const API_URL = 'https://api.themoviedb.org/3'
+
+module.exports = {
+  API_URL
+}
