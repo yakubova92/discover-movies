90a23a386d55f817043583f7bfb9e47e9a1c0ae7
Alla Yakubova <yakubova92@gmail.com>
2020-04-29 21:42:36 -0400

    added simple error handling for api calls

--- server/api/routes/movies.js
+++ server/api/routes/movies.js
@@ -5,24 +5,35 @@ const router = express.Router()
 const { API_URL } = require('../../constants')
 const { api_key } = require('../../secrets')
 
+const checkStatus = res => {
+  if (res.ok) return res
+  throw new Error(`Something went wrong: Received ${res.status} status from MovieDB Api`)
+}
+
 router.get('/', (req, res, next) => {
   const title = req.query.title
   return fetch(`${API_URL}/search/movie?api_key=${api_key}&language=en-US&query=${title}&page=1&include_adult=false`)
+    .then(checkStatus)
     .then(res => res.json())
-    .then(json => res.send(json));
+    .then(json => res.send(json))
+    .catch(err => console.error(err))
 })
 
 router.get('/popular', (req, res, next) => {
   return fetch(`${API_URL}/movie/popular?api_key=${api_key}&language=en-US&page=1`)
+    .then(checkStatus)
     .then(res => res.json())
-    .then(json => res.send(json));
+    .then(json => res.send(json))
+    .catch(err => console.error(err))
 })
 
 router.get('/:movieId', (req, res, next) => {
   const { movieId } = req.params
   return fetch(`${API_URL}/movie/${movieId}?api_key=${api_key}`)
+    .then(checkStatus)
     .then(res => res.json())
-    .then(json => res.send(json));
+    .then(json => res.send(json))
+    .catch(err => console.error(err))
 })
 
 module.exports = router
